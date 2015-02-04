6a9238e686069e34a4bb72e4843e3bbcc551e2b6
Stacy Fabian <stacy.fabian@gmail.com>
2015-02-03 18:56:20 -0800

    Adjust nav formatting

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// Initialize bootstrap carousel
+// ---- INITIALIZE BOOTSTRAP CAROUSEL ----
 $(document).ready(function(){
 	$('.carousel').carousel();
 });
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+// ---- PAGE SCROLLING ----
 $(function() {
     $('a.nav-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -13,3 +13,74 @@ $(function() {
         event.preventDefault();
     });
 });
+
+// ---- CONTACT FORM ----
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behavior
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "stacy.fabian@gmail.com", //insert url for AWS once working
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Thank you! Your message has been sent. You can expect a response within 24 hours.</strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that the mail server is not responding. Please try again later or find me via linkedin.");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+// When clicking on Full hide fail/success boxes
+$('#name').focus(function() {
+    $('#success').html('');
+}); 
